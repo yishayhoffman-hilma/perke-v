fd28fb097b5fad572d27f4cd4b08d697b9767a41
yishiehoffman-hilma <yishiehoffman@hilma.tech>
2025-11-23 15:19:06 +0200

    added get path to get files in a directory

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,26 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const path = require("path");
+const fs = require("fs");
 
+const USERS_ROOT_DIR = path.join(__dirname, "../users/");
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/:username", function (req, res, next) {
+  const username = req.params.username;
+  const relativeFilePath = username;
+
+  console.log(`Attempting to serve file: ${relativeFilePath}`);
+  console.log(`From root directory: ${USERS_ROOT_DIR}`);
+
+  fs.readdir(USERS_ROOT_DIR + username, (err, files) => {
+    if (err) {
+      res.send("bad path");
+      console.log(err);
+      return err;
+    } else {
+      res.json(files);
+    }
+  });
 });
 
 module.exports = router;
